ffd6581454a804da3c2f6149c3e798a58a46bc16
Logan Coates <coates.logan1125@gmail.com>
2020-09-30 14:36:39 -0600

    Added var and numeric function

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function isNumeric(num){
+  if (num.indexOf(".") > -1)
+  {
+    return false;
+  }
+
+  return !isNaN(num);
+
+}
+
+function generatePassword() {
+  var passwordStrn = "";
+  var characterCheckLength;
+  var characterCheck = "";
+  var lengthInt;
+  var lengthStr = window.prompt ("How many many characters do you want your password to have?")
+}
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
